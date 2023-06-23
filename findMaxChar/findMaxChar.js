86c82238a7c5a0e7a8a237260fd345b9d0bf9864
igor-senichev <96760301+igor-senichev@users.noreply.github.com>
2023-06-23 15:44:49 +0300

    Development (#1)
    * Created a folder with task solution findMaxChar
    
    * Update README.md
    
    * fix code review

---
+++ findMaxChar/findMaxChar.js
@@ -0,0 +1,19 @@
+function findMaxChar(str) {
+  const charCount = {};
+  let maxChar = '';
+  let maxCount = 0;
+
+  // Заполнение объекта charCount
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = charCount[char] ? ++charCount[char] : 1;
+
+    // Поиск максимально часто встречаемого символа
+    if (charCount[char] > maxCount) {
+      maxChar = char;
+      maxCount = charCount[char];
+    }
+  }
+
+  console.log(`${maxChar} - повторяется ${maxCount} раз(а)`);
+}
