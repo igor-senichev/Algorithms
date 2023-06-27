34fb3b7190cf406daeea9aac6cbead0bead309eb
Igor Senichev <senichev.igor@gmail.com>
2023-06-27 20:41:23 +0300

    Wrapped the code in a function calculateNumbers

--- even_and_odd_numbers.js
+++ even_and_odd_numbers.js
@@ -1,16 +1,20 @@
-let numbers = [];
-for (let i = 0; i < 10; i++) {
-  let randomNumber = Math.floor(Math.random() * 100) + 1;
-  numbers.push(randomNumber);
-}
-let evenNumbers = [];
-let oddNumbers = [];
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 === 0) {
-    evenNumbers.push(numbers[i]);
-  } else {
-    oddNumbers.push(numbers[i]);
+function calculateNumbers() {
+  let numbers = [];
+  for (let i = 0; i < 10; i++) {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    numbers.push(randomNumber);
   }
+  let evenNumbers = [];
+  let oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    } else {
+      oddNumbers.push(numbers[i]);
+    }
+  }
+  console.log(`Четные числа: ${evenNumbers}. Количество: ${evenNumbers.length}`);
+  console.log(`Нечетные числа: ${oddNumbers}. Количество: ${oddNumbers.length}`);
 }
-console.log(`Четные числа: ${evenNumbers}. Количество: ${evenNumbers.length}`);
-console.log(`Нечетные числа: ${oddNumbers}. Количество: ${oddNumbers.length}`);
+
+calculateNumbers();
